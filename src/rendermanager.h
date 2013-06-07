34efb4f4dc1a9574ec3b43e1e92a7e5b90e3d94a
Arkuwyr <adamrichter2@seznam.cz>
2013-06-07 10:07:02 +0200

    Update rendermanager.h
    Uprava pár funkcí

--- src/rendermanager.h
+++ src/rendermanager.h
@@ -5,31 +5,30 @@
 class RenderManager {
 
     public:
+    bool fullsc;   //hodnota kterou je vždy při zmenšení okna nastavit na 0 (vypnuty), jinak nebude fullscrean pracovat.
         RenderManager();
         void init();
         SDL_Surface *loadImage( std::string name );
         void applySurface( SDL_Surface *source, int offsetX, int offsetY, SDL_Rect * vyrez );
         void update() { SDL_Flip(screen); };
-        void resize(int x, int y);
-        void setFullscreen(){
+        
+        void resize(int x, int y){                       //podle poslaných hodnot zvětší /zmenší okno (již otevřené)
+        Window.Create (sf::VideoMode (x, y, 32),"XxY");
+        }
+        
+        void setFullscreen(bool fullsc){
 
-            if(fullsc) {
-
-                screen = SDL_SetVideoMode(scsize[0], scsize[1], 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
-                fullsc = false;
-            } else  {
-
-                screen = SDL_SetVideoMode(scsize[0], scsize[1], 32, SDL_HWSURFACE | SDL_DOUBLEBUF | SDL_FULLSCREEN);
-                fullsc = true;
+            if (fullsc == false){
+                Window.Create (sf::VideoMode (800, 600, 32), "Fullscreen", sf::Style::Fullscreen); //při volání funkce se rozhodne zdali už okno není zvětšno
+            fullsc = true;                                                                         // pokud není tak tak učiní, brání tak několikanásovnýmu fullsreenu
             }
-        };
+        }
 
         int* getScsize() {return scsize;};
 
 
     private:
         int scsize[2];
-        bool fullsc;
         SDL_Surface *screen;
 };
 
